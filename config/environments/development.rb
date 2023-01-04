d4a74b1d9be50de8f1c4cbe95d41c59a12a6b225
Guillaume Briday <guillaumebriday@gmail.com>
2023-01-03 20:32:27 -0500

    Configuring Redis cache store

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
@@ -34,6 +33,8 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+
+  config.cache_store = :redis_cache_store, { url: 'redis://localhost:6379/0' }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
