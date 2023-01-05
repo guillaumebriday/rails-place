96fcb55a79fadbd9383da8f3449c408eb482e639
Guillaume Briday <guillaumebriday@gmail.com>
2023-01-04 23:37:11 -0500

    Improving UI

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,40 @@
 # frozen_string_literal: true
 
 MAP_SIZE = 50
-MAP_COLORS = %w[gray red yellow green blue violet pink white].freeze
+
+# Official colors of r/Place 2022 edition.
+# See: https://en.wikipedia.org/wiki/R/place#2022_experiment
+MAP_COLORS = %w[
+  #6d001a
+  #be0039
+  #ff4500
+  #ffa800
+  #ffd635
+  #fff8b8
+  #00a368
+  #00cc78
+  #7eed56
+  #00756f
+  #009eaa
+  #00ccc0
+  #2450a4
+  #3690ea
+  #51e9f4
+  #493ac1
+  #6a5cff
+  #94b3ff
+  #811e9f
+  #b44ac0
+  #e4abff
+  #de107f
+  #ff3881
+  #ff99aa
+  #6d482f
+  #9c6926
+  #ffb470
+  #000000
+  #515252
+  #898d90
+  #d4d7d9
+  #ffffff
+].freeze
